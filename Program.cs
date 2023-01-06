0ca9a410e7d9da88440e603d93073c9af77124bc
Paulo <trigopaulo@gmail.com>
2023-01-06 11:40:31 -0300

    adicionando tabuleiro

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using tabuleiro;
+
+namespace xadrez_console{
+    static void Main(string[] args){
+
+        Posicao P;
+
+        P = new Posicao(3,4);
+
+        Console.WriteLine("Posição: "+ P);
+
+        Console.ReadLine();
+    }
+}
