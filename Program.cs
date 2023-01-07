f4755506c76926c1fb5050e7cb538f6709834db8
Paulo <trigopaulo@gmail.com>
2023-01-07 11:51:39 -0300

    fim do projeto

--- Program.cs
+++ Program.cs
@@ -1,15 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
+﻿using System;
 using tabuleiro;
+using xadrez;
 
+namespace xadrez_console {
+    class Program {
+        static void Main(string[] args) {
 
-namespace xadrez_console{
-    class Program{
-        static void Main(string[] args){
+            try {
+                PartidaDeXadrez partida = new PartidaDeXadrez();
 
-            Tabuleiro tab = new Tabuleiro(8,8);
+                while (!partida.terminada) {
 
-            Tela.imprimirTabuleiro(tab);
+                    try {
+                        Console.Clear();
+                        Tela.imprimirPartida(partida);
+
+                        Console.WriteLine();
+                        Console.Write("Origem: ");
+                        Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                        partida.validarPosicaoDeOrigem(origem);
+
+                        bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+
+                        Console.Clear();
+                        Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+
+                        Console.WriteLine();
+                        Console.Write("Destino: ");
+                        Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+                        partida.validarPosicaoDeDestino(origem, destino);
+
+                        partida.realizaJogada(origem, destino);
+                    }
+                    catch (TabuleiroException e) {
+                        Console.WriteLine(e.Message);
+                        Console.ReadLine();
+                    }
+                }
+                Console.Clear();
+                Tela.imprimirPartida(partida);
+            }
+            catch (TabuleiroException e) {
+                Console.WriteLine(e.Message);
+            }
 
             Console.ReadLine();
         }
